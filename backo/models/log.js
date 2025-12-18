ff532ee2285b7a6afa8d784fc72f1b8873576ee2
Arshath <elyasarshath6963gmail.com>
2025-12-18 12:01:54 +0530

    clearing the server bug

--- backo/models/log.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const LogSchema = new mongoose.Schema({
-    type: { type: String, required: true }, // e.g., 'SERVER_START'
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Log', LogSchema);
