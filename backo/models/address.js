af28ca048db6c0ff286804a1528546f0ec5901f9
Arshath <elyasarshath6963gmail.com>
2025-12-28 22:12:49 +0530

    Fix Address model import in auth login

--- backo/models/address.js
+++ backo/models/address.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
+const address = require('../models/address');
 
 
 const addressSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  name: { type: String, required: true },
-  phone: { type: String, required: true },
-  street: { type: String, required: true },
-  city: { type: String, required: true },
-  pincode: { type: String, required: true },
-  isDefault: { type: Boolean, default: false }
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    pincode: { type: String, required: true },
+    isDefault: { type: Boolean, default: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Address', addressSchema);
