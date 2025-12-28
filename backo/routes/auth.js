c3303d8f48f47fbd6d96a36a0be46527f6ffe283
Arshath <elyasarshath6963gmail.com>
2025-12-28 12:39:24 +0530

    add auth.js

--- backo/routes/auth.js
+++ backo/routes/auth.js
@@ -48,7 +48,8 @@ router.post('/login', async (req, res) => {
                 id: user._id,
                 name: user.name,
                 email: user.email
-            }
+            },
+            addresses
         });
 
     } catch (err) {
