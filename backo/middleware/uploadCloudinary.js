d1d4f0e672f6692a3a90a6e2350eea4c819f0b65
Arshath <elyasarshath6963gmail.com>
2026-01-11 19:13:20 +0530

    middleware/cloudinary change

--- backo/middleware/uploadCloudinary.js
+++ backo/middleware/uploadCloudinary.js
@@ -2,12 +2,17 @@ const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('../config/cloudinary');
 
+
+if (!cloudinary || !cloudinary.uploader) {
+  throw new Error('Cloudinary is not properly configured');
+}
+
 const storage = new CloudinaryStorage({
-    cloudinary,
-    params: {
-        folder: 'baco-clothing',
-        allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
-    },
+  cloudinary,
+  params: async (req, file) => ({
+    folder: 'baco-clothing',
+    format: file.mimetype.split('/')[1], // jpg, png, webp
+  }),
 });
 
 module.exports = multer({ storage });
