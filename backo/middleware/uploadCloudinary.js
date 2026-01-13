601d52ddf7f105336fad4e1523f5f5ac891021b1
Arshath <elyasarshath6963gmail.com>
2026-01-13 22:13:13 +0530

    cloudinary bug fixing...

--- backo/middleware/uploadCloudinary.js
+++ backo/middleware/uploadCloudinary.js
@@ -2,12 +2,23 @@ const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('../config/cloudinary');
 
+console.log('🧪 UPLOAD MIDDLEWARE CHECK');
+console.log('cloudinary object:', cloudinary);
+console.log('cloudinary.uploader:', cloudinary?.uploader);
+
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: 'baco-clothing',
-        allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
-    },
+  cloudinary,
+  params: (req, file) => {
+    console.log('🧪 MULTER PARAMS CALLED');
+    console.log('file:', file?.originalname, file?.mimetype);
+
+    return {
+      folder: 'backo-clothing',
+      allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
+    };
+  },
 });
 
-module.exports = multer({ storage });
+const upload = multer({ storage });
+
+module.exports = upload;
