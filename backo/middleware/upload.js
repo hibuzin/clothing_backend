9ef712f8c1080faa3d67bc0810c6faec96be4fbc
Arshath <elyasarshath6963gmail.com>
2026-02-06 16:15:49 +0530

    cloudinary error clear

--- backo/middleware/upload.js
+++ backo/middleware/upload.js
@@ -5,7 +5,7 @@ const cloudinary = require('../config/cloudinary');
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'backo', // cloudinary folder name
+    folder: 'products', // cloudinary folder name
     allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
   },
 });
